6d998d399150c701524f151471eadb251e0e44d8
Muhammad Anas Muhammad Luqman <anasluqman609@gmail.com>
2026-07-22 20:08:53 +0500

    Create types of Operators.py

---
+++ types of Operators.py
@@ -0,0 +1,33 @@
+#Arithematic operator
+a=25
+b=30
+print(a+b)
+print(a-b)
+print(a%b)#remainder
+print(a*b)
+print(a**b)#a^b
+
+#Relational Operators
+a=50
+b=30
+print(a == b)
+print(a != b)
+print(a<=b)
+print(a<b)
+print(a>=b)
+print(a>b)
+
+#Assignemnet Operators
+num=10
+num +=10
+print("num :",num)
+num -=10
+print("num :",num)
+num *=10
+print("num :",num)
+num /=10
+print("num :",num)
+num %=10
+print("num :",num)
+num **=10
+print("num :",num)
