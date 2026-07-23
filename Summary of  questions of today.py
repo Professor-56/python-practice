7085fb1a84538c5ac9d77aa6a2236106aa9d75be
Muhammad Anas Muhammad Luqman <anasluqman609@gmail.com>
2026-07-23 11:57:07 +0500

    input_statement.py

---
+++ Summary of  questions of today.py
@@ -0,0 +1,22 @@
+#write a program to input 2 numbers & print their sum.
+#num1= int(input("enter number 1:"))
+#num2=int(input("enter  number 2:"))
+#print ("sum :",num1+num2)
+
+#Write a program to input side of square & print it's area.
+#side=float(input("enter side of square :"))
+
+#print("area of the square =", side ** side)
+
+#write a program to input 2 floating point numbers & print their average 
+
+#num1=float(input("enter the first floating point"))
+#num2=float(input("enter the second floating point"))
+#print("avg =",(num1+num2)/2)
+
+#Write a program to input 2 int numbers, a and b.
+#print True if a is greater than or equal to b . if not print False.
+
+a=int(input("int first number"))
+b=int(input("int second number"))
+print(a>=b)
